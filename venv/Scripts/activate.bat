d8f388809460973dd0faa5ad0cd72deb58416e92
Shankar Lohar <shankarloharmail@gmail.com>
2021-11-17 23:51:29 +0530

    first FLASK app

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=A:\WorkFolder\GitHub Projects\FlaskCRUDapp\venv
+set VIRTUAL_ENV=A:\WorkFolder\GitHub Projects\FlaskWithCRUD-TODO\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
